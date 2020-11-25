dcc2e951f863c7a297f29105fe6554483078a7e9
Shakya, Kajol <kajol.shakya@verscend.com>
2020-11-25 13:03:33 +0545

    added variable

---
+++ VariableDeclarations.js
@@ -0,0 +1,15 @@
+//Datatypes
+
+// Primary
+var a = "kajol";
+var b = true;
+var c = 3453;
+var d = 2n ** 200n;
+var e = undefined;
+var f = null;
+
+//Structural
+var g = [2, 3, 4, 5];
+var h = { name: "kajol", isFemale: true };
+
+console.log(h);
