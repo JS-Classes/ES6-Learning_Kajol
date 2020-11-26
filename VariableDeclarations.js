138242d9ec3dae14d925b0b85d4490b24bfedce2
Shakya, Kajol <kajol.shakya@verscend.com>
2020-11-26 09:19:54 +0545

    learned about datatypes

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -1,15 +1,34 @@
 //Datatypes
 
-// Primary
+// Primitive
+//var a = "kajol";
 var a = "kajol";
 var b = true;
-var c = 3453;
+var c = 345.6;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; //Not a number
+var x = "dfsdf"/34;
+//console.log(x);
+
+//console.log(Number.MAX_VALUE);
+//console.log(Number.MIN_VALUE);
 var d = 2n ** 200n;
+
 var e = undefined;
 var f = null;
 
 //Structural
 var g = [2, 3, 4, 5];
+var i =["a", "b", "c"];
 var h = { name: "kajol", isFemale: true };
 
-console.log(h);
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof i);
