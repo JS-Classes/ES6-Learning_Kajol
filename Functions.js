f64ee1e21921c88af04d91a379038e899a0ca7e5
Shakya, Kajol <kajol.shakya@verscend.com>
2020-11-26 10:06:53 +0545

    learned about functions

---
+++ Functions.js
@@ -0,0 +1,22 @@
+//anynomous
+var fun1 = function (number)
+{
+    let squared = number * number;
+    return squared;
+}
+let s1 = fun1(345);
+console.log(s1);
+
+
+//function expression
+function square(number)
+{
+    let squared = number * number;
+    return squared;
+}
+
+
+let s = square(345);
+console.log(s);
+
+//Arrow function
