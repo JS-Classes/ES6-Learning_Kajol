5b6fa3ab4a606c1bb7c5749cfc7e822b829cf61a
Shakya, Kajol <kajol.shakya@verscend.com>
2020-12-03 14:50:31 +0545

    Learned Construct and exercise for multiplication.

---
+++ Exercises.js
@@ -0,0 +1,47 @@
+//1. Program to print multiplication table for a number 
+
+const number=3;
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+//2. Program to print multiplication table for a number upto some range (default should be 10)
+
+let y = 2;
+function MULTIPLY (y, range = 5) {
+    
+   for (let x = 1; x<= 10; x++){
+       let mul = y*x;
+       console.log(`${y} * ${x} = ${mul}`);
+       if(x == range)
+       {
+            break;
+       }
+    }
+}
+MULTIPLY(y,2);
+
+//3. Program to print multiplication table for numbers from m to n.
+
+function MultiplicationTable(m,y, range = 10)
+{
+    for(let i = m; i<= y; i++)
+    {
+        console.log(`Multiplication Table for ${i}`)
+        for(let x = 1; x<=range; x++)
+        {
+            let mul = m*x;
+            console.log(`${i} * ${x} = ${mul}`);
+        }
+    }
+}
+
+MultiplicationTable(2,5);
+
+
+
+
+
+
+
