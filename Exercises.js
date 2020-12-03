e3375c76818a06ab17d925af9dd5b82ac249a2b8
Shakya, Kajol <kajol.shakya@verscend.com>
2020-12-03 14:55:05 +0545

    modified multipication table

--- Exercises.js
+++ Exercises.js
@@ -24,9 +24,9 @@ MULTIPLY(y,2);
 
 //3. Program to print multiplication table for numbers from m to n.
 
-function MultiplicationTable(m,y, range = 10)
+function MultiplicationTable(m,n, range = 10)
 {
-    for(let i = m; i<= y; i++)
+    for(let i = m; i<= n; i++)
     {
         console.log(`Multiplication Table for ${i}`)
         for(let x = 1; x<=range; x++)
